7d6b3a53b739f06cc06a2c01d2562e7a87d7f6dd
Melissa M. Dailey <mel.dailey@gmail.com>
2016-03-13 15:09:26 -0700

    finished overall structure and images

---
+++ img/warm_files/config-6e774e4c484879651f5ec8bf1a75f50d4f4a2667340e97ed237398f818ca797c.js
@@ -0,0 +1,9 @@
+(function(){E.config.isProd=!0
+E.config.isIntegration=!1
+E.config.assetHost="//d1zvwc1xfc8qp0.cloudfront.net"
+E.config.bugsnagJsApiKey="bd6e5bf9aae71149c3a2d9ef8e8169fd"
+E.config.braintreeClientKey="MIIBCgKCAQEAvx/K85qQ3GP4SEXv3L9BSc/j4Yg1LBRT92zfEZAlMWx1IYDvJyoOlUZTzvynpA2y+/QkYlrM9M6+ummcxo8FbywccR1YuAJmmMho8W9Oq0iw/U52mLXBcojzuiO2h3QrBOWdvetojP9XfAhP5ls7jM59a07qVsJ2JZO9kTb86shTFZ3JkRPA7Y9HScYt9PU7r9etyirJKMIOpZng3fmnt+f4wUt+YJd5VqNWqez8q4DO1W0w5DJKrLImZVob6K19L3hREf6ZeAaGYMknlFPzQCfyH0j/RrJaXS1XOcZkfgRAOcLpIsXOTFNF/MUL7kO0w0IhDB5zbUIUL1YIQCVOBQIDAQAB"
+E.config.braintreeRedirectURL="https://api.braintreegateway.com:443/merchants/pcq4d87vzp55pr28/transparent_redirect_requests"
+E.config.messengerApiKey="691107797673547"
+!function(){var e
+return e="1.5"}()}).call(this)
